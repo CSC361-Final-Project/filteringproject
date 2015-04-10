82428d914af77f94235f3e9f1a52717a7aa6f32f
kincbe10 <kincbe10@KINCBE10-2014.deacnet.wfu.edu>
2015-04-09 20:48:23 -0400

    initial commit of fourier transform and complex number classes

---
+++ src/filtering/ComplexNumber.java
@@ -0,0 +1,253 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package filtering;
+
+/**
+ *
+ * @author kincbe10
+ */
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ *
+ * @author kincbe10
+ * Complex Number java class for use in Fourier and Convolution
+ */
+public class ComplexNumber {
+    /******************************************
+     * 
+     * Private member attributes
+     * 
+     ******************************************/
+    //member attributes for real and complex parts of #
+    public final double real;
+    public final double imag;
+  //  private DecimalFormat df = new DecimalFormat("###.##");
+    /******************************************
+     * 
+     * Constructors
+     * 
+     * @param r
+     * @param i
+     ******************************************/
+    //basic constructor
+    public ComplexNumber(double r, double i){
+        this.real = /*Math.round(r*1000.0)/1000.0;*/ r;
+        this.imag = /*Math.round(i*1000.0)/1000.0;*/ i;
+    
+    }
+    /******************************************
+     * 
+     * Important manipulations
+     * 
+     * @return 
+     ******************************************/
+    //return absolute value of complex number---sqrt(r*r+i*i) as a double
+    public double magnitudeDouble(){
+        return Math.sqrt(this.real*this.real + this.imag*this.imag);
+    }
+    //return magnitude as a ComplexNumber
+    public ComplexNumber magnitude(){
+        return new ComplexNumber(this.magnitudeDouble(),0);
+    }
+    //uses Math.atan2 to return theta from polar coord/phase
+    public double getTheta(){
+        return Math.atan2(imag, real);
+    }
+    //return conjugate complex number of the form real, -imaginary
+    public ComplexNumber conjugate(){
+        return new ComplexNumber(this.real, -this.imag);
+    }
+    //returns reciprocal (real/scale, -imaginary/scale
+    public ComplexNumber reciprocal(){
+        return new ComplexNumber(this.real/(this.real*this.real+this.imag*this.imag), -this.imag/(this.real*this.real+this.imag*this.imag));
+    }
+    public String complexNumberToString(){
+        if (this.imag == 0) return this.real + "";
+        if (this.real == 0) return this.imag + "i";
+        if (this.imag <  0) return this.real + " - " + (-this.imag) + "i";
+        return this.real + " + " + this.imag + "i";
+    }
+    /******************************************
+     * 
+     * Arithmetic
+     * 
+     * @param t
+     * @return 
+     ******************************************/
+    //addition of complex numbers
+    public ComplexNumber add(ComplexNumber t){
+        double r = this.real+t.real;
+        double i = this.imag+t.imag;
+        return new ComplexNumber(r, i);
+    }
+    //addition of a complex number and a double
+    public ComplexNumber add(double d){
+        return new ComplexNumber(d+this.real, this.imag);
+    }
+    //subtraction of complex numbers
+    public ComplexNumber subtract(ComplexNumber t){
+        double r = this.real-t.real;
+        double i = this.imag-t.imag;
+        return new ComplexNumber(r, i);
+    }
+    //subtraction of a complex number and a double
+    public ComplexNumber subtract(double d){
+        return new ComplexNumber(this.real-d, this.imag);
+    }
+    //multiplication of two complex numbers
+    public ComplexNumber multiply(ComplexNumber t){
+        double r = this.real*t.real - this.imag*t.imag;
+        double i = this.real*t.imag + this.imag*t.imag;
+        return new ComplexNumber(r,i);
+    }
+    //multiplication of a complex number by a double
+     public ComplexNumber multiply(double d) {
+        return new ComplexNumber(this.real*d, this.imag*d);
+    }
+    //division via multiplication by the reciprocal
+    public ComplexNumber divide(ComplexNumber t){
+        return this.multiply(t.reciprocal());
+    }
+    
+    //division of a complex number by a double
+    public ComplexNumber divide(double d){
+        return new ComplexNumber(this.real/d, this.imag );
+    }
+    
+    //exp function get complex number x^this
+    public ComplexNumber exp(){
+        return new ComplexNumber(Math.exp(this.real)*Math.cos(this.imag), Math.exp(this.real)*Math.sin(this.imag));
+    }
+        /******************************************
+     * 
+     * Trig
+     * 
+     ******************************************/
+    
+    // return sine of a complex number
+
+    /**
+     * Trig
+     * @return
+     */
+        public ComplexNumber sin() {
+        return new ComplexNumber(Math.sin(real) * Math.cosh(imag), Math.cos(real) * Math.sinh(imag));
+    }
+
+    // return cosine of a complex number
+    public ComplexNumber cos() {
+        return new ComplexNumber(Math.cos(real) * Math.cosh(imag), -Math.sin(real) * Math.sinh(imag));
+    }
+
+    // return tangent of a complex number
+    public ComplexNumber tan() {
+        return sin().divide(cos());
+    }
+    
+     
+    public static ComplexNumber[][] matrixMultiply(ComplexNumber[][] A, ComplexNumber[][] B){
+        int mA = A.length;
+        int nA = A[1].length;
+        int mB = B.length;
+        int nB = B[1].length;
+        if(A[0].length != B[0].length){
+            throw new RuntimeException("Illegal Matrices");
+        }
+        ComplexNumber[][] C = new ComplexNumber[mA][nB];
+        for(int m = 0; m < mA; m++){
+            for(int n = 0; n < nB; n++){
+                C[m][n] = new ComplexNumber(0,0);
+            }
+        }
+        for(int i = 0; i < mA; i++){
+            for(int j = 0; j < nB; j++){
+                for(int k = 0; k < nA; k++ ){
+                    C[i][j] = C[i][j].add(A[i][k].multiply(B[k][j]));
+                }
+            }
+        }
+        return C;
+    }
+    public static ComplexNumber[] matrixVectorMultiply(ComplexNumber[][]A, double[] D ){
+        if(D.length != A.length){
+            throw new RuntimeException("Illegal matrices");
+        }
+        ComplexNumber[] B = new ComplexNumber[A.length];
+        for (int w = 0; w < B.length; w++){
+            B[w] = new ComplexNumber(0,0);
+        }
+        for(int i = 0; i < A.length; i++){
+            for(int j = 0; j < A[0].length; j++){
+                B[j] = B[j].add(A[i][j].multiply(D[i]));
+            }
+        }
+        return B;
+    }
+        public static ComplexNumber[] matrixVectorMultiply(ComplexNumber[][]A, ComplexNumber[] D ){
+        if(D.length != A.length){
+            throw new RuntimeException("Illegal matrices");
+        }
+        ComplexNumber[] B = new ComplexNumber[A.length];
+        for (int w = 0; w < B.length; w++){
+            B[w] = new ComplexNumber(0,0);
+        }
+        for(int i = 0; i < A.length; i++){
+            for(int j = 0; j < A[0].length; j++){
+                B[j] = B[j].add(A[i][j].multiply(D[i]));
+            }
+        }
+        return B;
+    }
+
+    public static void printComplexMatrix(ComplexNumber[][] F){
+        for (ComplexNumber[] F1 : F) {
+            System.out.print("||");
+            for (ComplexNumber F11 : F1) {
+                String ij = F11.complexNumberToString();
+                int whiteSpace = 17-ij.length();
+                System.out.print(" "+ij);
+                for(int w = 0; w < whiteSpace; w++){
+                    System.out.print(" ");
+                }
+                System.out.print(" |");
+            }
+            System.out.print("|");
+            System.out.println();
+        }
+    }
+    public static void printComplexMatrixCSV(ComplexNumber[][] F){
+        for (ComplexNumber[] F1 : F) {
+            for (int j = 0; j < F1.length; j++) {
+                String ij = F1[j].complexNumberToString();
+                if (j < F1.length - 1) {
+                    System.out.print(ij+", ");
+                } else {
+                    System.out.print(ij);
+                }                    
+            }
+            System.out.println();
+        }
+    }
+    public static void printComplexVectorCSV(ComplexNumber[]F){
+        for(int i = 0; i < F.length; i++){
+           
+               // String ij = F[i].complexNumberToString();
+                    System.out.println(i+", "+F[i].magnitude());
+                    
+            
+            System.out.println();
+        }
+    }
+        
+
+//    public String complexNumberToString() {
+//        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+//    }
+}
---
+++ src/filtering/Fourier.java
@@ -0,0 +1,272 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package filtering;
+
+import static java.lang.System.arraycopy;
+
+/**
+ *
+ * @author kincbe10
+ * Class of static methods to perform Fourier Transforms and 
+ * Inverse Fourier Transforms on a polynomial/signal
+ * 
+ */
+public class Fourier {
+    
+    /******************************************
+     * 
+     * Methods to build Fourier matrices and inverse Fourier matrices of size N 
+     * for Fourier Transform by matrix vector multiplication
+     * using methods from ComplexNumber class
+     * 
+     * @param N
+     * @return 
+     ******************************************/
+    
+    /*Generate Fourier matrix by calculating real and imaginary components
+      for each coordinate ij
+    */
+    public static ComplexNumber[][] fourierMatrix(int N){
+        ComplexNumber[][] F = new ComplexNumber[N][N];
+        for(int i = 0; i < N; i++){
+            for(int j = 0; j < N; j++){
+                double r = Math.cos((-2*Math.PI*i*j)/N);
+                double im = Math.sin((-2*Math.PI*i*j)/N);
+                F[i][j] = new ComplexNumber(r,im);
+            }
+        }
+        return F;
+    }
+    
+    /*
+      Generate inverse Fourier matrix by multiplying the conjugate of each
+       position in the Fourier matrix by 1/N
+    */
+    public static ComplexNumber[][] inverseFourierMatrix(int N){
+           ComplexNumber[][] F = fourierMatrix(N);
+           ComplexNumber[][] G = new ComplexNumber[F.length][F.length];
+           for(int i = 0; i < F.length; i++){
+               for(int j = 0; j < F[0].length; j++){
+                   G[i][j] = F[i][j].conjugate().multiply(1.0/N);
+           }
+           }
+           return G;
+           
+
+    }
+     /******************************************
+     * 
+     * Methods to compute Discrete Fourier Transform and Inverse Fourier Transform by
+     * matrix vector multiplication of a signal/polynomial by Fourier Matrix
+     * or Inverse Fourier Matrix.
+     * 
+     * Returns a vector that is the result of multiplying signal/coefficient
+     * vector by Fourier or Inverse Fourier matrix
+     * 
+     * Support for both ComplexNumber and double vectors
+     * 
+     * Expected O(n^2) efficiency
+     * 
+     * @param S
+     * @return 
+     ******************************************/
+    
+    /*Calculate Discrete Fourier Transform by multiplying coefficient vector by Fourier
+     *Matrix of the same length
+     */
+    
+    //Matrix DFT
+    
+    //ComplexNumber param
+    public static ComplexNumber[] matrixVectorDFT(ComplexNumber[] S){
+        ComplexNumber[][] FM = fourierMatrix(S.length);
+        ComplexNumber[] res = ComplexNumber.matrixVectorMultiply(FM, S);
+        return res;
+    
+    }
+    
+    //Double param
+    public static ComplexNumber[] matrixVectorDFT(double[] S){
+        ComplexNumber[][] FM = fourierMatrix(S.length);
+        ComplexNumber[] res = ComplexNumber.matrixVectorMultiply(FM, S);
+        return res;
+    
+    }
+        
+    //Matrix IFT
+        
+    //ComplexNumber param
+    public static ComplexNumber[] matrixVectorIFT(ComplexNumber[] S){
+        ComplexNumber[][] FM = inverseFourierMatrix(S.length);
+        ComplexNumber[] res = ComplexNumber.matrixVectorMultiply(FM, S);
+        return res;
+    
+    }
+    
+    //Double param
+    public static ComplexNumber[] matrixVectorIFT(double[] S){
+        ComplexNumber[][] FM = inverseFourierMatrix(S.length);
+        ComplexNumber[] res = ComplexNumber.matrixVectorMultiply(FM, S);
+        return res;
+    
+    }
+    
+     /******************************************
+     * 
+     * Methods to compute Fast Fourier Transform and Inverse Fast Fourier Transform
+     * 
+     * Returns a vector that is the result of computing FFT or IFFT on input
+     * polynomial/signal
+     * 
+     * Support for both ComplexNumber and double vectors
+     * 
+     * Expected O(nlogn) efficiency
+     * 
+     * @param S
+     * @return 
+     ******************************************/
+    
+    //FFT w/ ComplexNumber Param only works on matrices where length%2 = 0
+    public static ComplexNumber[] fft(ComplexNumber[] S){
+        //base case
+        if(S.length == 1){
+            ComplexNumber[] bc = new ComplexNumber[1];
+            arraycopy(S, 0, bc, 0, 1);
+            return bc;
+        }
+        //error checking for vector length--> must be a power of 2
+        if(S.length%2 != 0){
+            throw new RuntimeException("Coefficient vector length must be a power of 2 to use FFT");
+        }
+        //array to hold even input coefficients
+        ComplexNumber[] E = new ComplexNumber[S.length/2];
+        //take coefficients at 2*i to get even indexed values from input
+        for(int i = 0; i < E.length; i++ ){
+            E[i] = S[i*2];
+        }
+        //recurse on evens
+        ComplexNumber[] ER = fft(E);
+        
+        //array to hold odd input coefficients
+        ComplexNumber[] O = new ComplexNumber[S.length/2];
+        //take coefficients at 2*k+1 to get odd indexed values from input
+        for(int k = 0; k < E.length; k++ ){
+            O[k] = S[k*2+1];
+        }
+        //recurse on odds
+        ComplexNumber[] OR = fft(O);
+        
+        //array to hold result
+        ComplexNumber[] res = new ComplexNumber[S.length];
+        //combine computing transform at each index
+        for(int j = 0; j < S.length/2; j++){
+            //calc Fourier Multiple for each index
+            ComplexNumber temp = new ComplexNumber(Math.cos(-2*j*Math.PI/S.length), Math.sin(-2*j*Math.PI/S.length));
+            //add sum of even and temp*odd to first half of output
+            res[j] = ER[j].add(temp.multiply(OR[j]));
+            //add difference of even and temp*odd to second half of output
+            res[j + S.length/2] = ER[j].subtract(temp.multiply(OR[j]));
+        }
+        
+        return res;
+    }
+    
+    //FFT w/ Double Param only works on matrices where length%2 = 0
+    public static ComplexNumber[] fft(double[] S){
+        //base case
+        if(S.length == 1){
+            ComplexNumber[] bc = new ComplexNumber[1];
+            arraycopy(S, 0, bc, 0, 1);
+        }
+        //error checking for vector length--> must be a power of 2
+        if(S.length%2 != 0){
+            throw new RuntimeException("Coefficient vector length must be a power of 2 to use FFT");
+        }
+        //array to hold even input coefficients
+        ComplexNumber[] E = new ComplexNumber[S.length/2];
+        //take coefficients at 2*i to get even indexed values from input
+        for(int i = 0; i < E.length; i++ ){
+            E[i] = new ComplexNumber(S[i*2], 0);
+        }
+        //recurse on evens
+        ComplexNumber[] ER = fft(E);
+        
+        //array to hold odd input coefficients
+        ComplexNumber[] O = new ComplexNumber[S.length/2];
+        //take coefficients at 2*k+1 to get odd indexed values from input
+        for(int k = 0; k < E.length; k++ ){
+            O[k] = new ComplexNumber(S[k*2+1], 0);
+        }
+        //recurse on odds
+        ComplexNumber[] OR = fft(O);
+        
+        //array to hold result
+        ComplexNumber[] res = new ComplexNumber[S.length];
+        //combine computing transform at each index
+        for(int j = 0; j < S.length/2; j++){
+            //calc Fourier Multiple for each index
+            ComplexNumber temp = new ComplexNumber(Math.cos(-2*j*Math.PI/S.length), Math.sin(-2*j*Math.PI/S.length));
+            //add sum of even and temp*odd to first half of output
+            res[j] = ER[j].add(temp.multiply(OR[j]));
+            //add difference of even and temp*odd to second half of output
+            res[j + S.length/2] = ER[j].subtract(temp.multiply(OR[j]));
+        }
+        
+        return res;
+    }
+    
+    /*Compute IFFT by taking conjugate of input computing fft, 
+     *then the conjugate of that result and multiplying each 
+     *entry by 1/N
+    */
+    //IFFT w/ ComplexNumber Param only works on matrices where length%2 = 0
+    public static ComplexNumber[] ifft(ComplexNumber[] S){
+        //error checking for vector length--> must be a power of 2
+        if(S.length%2 != 0){
+            throw new RuntimeException("Coefficient vector length must be a power of 2 to use FFT");
+        }
+        //array to hold output
+        ComplexNumber[] res = new ComplexNumber[S.length];
+        for(int i = 0; i < S.length; i++ ){
+            res[i] = S[i].conjugate();
+        }
+        
+        //call fft
+        res = fft(res);
+        
+        //take conjugate and divide by n
+        for(int j = 0; j < S.length; j++ ){
+            ComplexNumber temp = res[j].conjugate();
+            res[j] = res[j].divide((double)S.length);
+        }
+        
+        return res;
+    }
+    
+    //IFFT with double param only works on matrices where length%2 = 0
+    public static ComplexNumber[] ifft(double[] S){
+        //error checking for vector length--> must be a power of 2
+        if(S.length%2 != 0){
+            throw new RuntimeException("Coefficient vector length must be a power of 2 to use FFT");
+        }
+        //array to hold output
+        ComplexNumber[] res = new ComplexNumber[S.length];
+        for(int i = 0; i < S.length; i++ ){
+            res[i] = new ComplexNumber(1/S[i], 0);
+        }
+        
+        //call fft
+        res = fft(res);
+        
+        //take conjugate and divide by n
+        for(int j = 0; j < S.length; j++ ){
+            ComplexNumber temp = res[j].conjugate();
+            res[j] = res[j].divide((double)S.length);
+        }
+        
+        return res;
+    }
+}
